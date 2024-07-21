d3bf0622f85356bfe848d67b4e9d33718db34c33
Shivam <shivamsorout8814@gmail.com>
2024-07-21 16:47:01 +0530

    list in python

---
+++ List.py
@@ -0,0 +1,16 @@
+# List diclarartion - 
+student = ['Shivam', 85, 'Prince']
+print(student)
+print(type(student))
+print(student[1])
+student[1] = 90
+print(student)
+# list are mutable as we see above we changed 1st element in list 
+
+str = "JavaDeveloper"
+print(str)
+# str[1] = 'b'
+# print(str)
+
+# String are immutable as we see above when try to change in string str[1] = 'b'. It gave error 
+# TypeError: 'str' object does not support item assignment
